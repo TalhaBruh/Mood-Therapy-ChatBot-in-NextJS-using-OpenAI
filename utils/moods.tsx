94412260dcf86715512ce9b4564cdecb2e8def1e
Talha Hussain <talhahussain349@gmail.com>
2024-05-24 17:36:10 +0500

    Add predefined mood types
    - Defined MoodType to structure mood options
    - Added a list of mood types with id and mood properties

---
+++ utils/moods.tsx
@@ -0,0 +1,25 @@
+type MoodType = {
+  id: number;
+  mood: string;
+};
+
+const moods: MoodType[] = [
+  {
+    id: 1,
+    mood: "angry",
+  },
+  {
+    id: 2,
+    mood: "Sad",
+  },
+  {
+    id: 3,
+    mood: "Happy",
+  },
+  {
+    id: 4,
+    mood: "Depressed",
+  },
+];
+
+export default moods;
