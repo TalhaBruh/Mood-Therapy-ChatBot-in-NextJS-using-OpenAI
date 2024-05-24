f99ab82c73af53886fdd75060b550f7c5d661821
Talha Hussain <talhahussain349@gmail.com>
2024-05-24 17:36:48 +0500

    Add predefined rating types
    - Defined RatingType to structure rating options
    - Added a list of rating types with id, rating, and title properties

---
+++ utils/rating.tsx
@@ -0,0 +1,60 @@
+type RatingType = {
+  id: number;
+  rating: number;
+  title: string;
+};
+
+const ratings: RatingType[] = [
+  {
+    id: 1,
+    rating: 10,
+    title: "10 out of 10",
+  },
+  {
+    id: 2,
+    rating: 9,
+    title: "9 out of 10",
+  },
+  {
+    id: 3,
+    rating: 8,
+    title: "8 out of 10",
+  },
+  {
+    id: 4,
+    rating: 7,
+    title: "7 out of 10",
+  },
+  {
+    id: 5,
+    rating: 6,
+    title: "6 out of 10",
+  },
+  {
+    id: 6,
+    rating: 5,
+    title: "5 out of 10",
+  },
+  {
+    id: 7,
+    rating: 4,
+    title: "4 out of 10",
+  },
+  {
+    id: 8,
+    rating: 3,
+    title: "3 out of 10",
+  },
+  {
+    id: 9,
+    rating: 2,
+    title: "2 out of 10",
+  },
+  {
+    id: 10,
+    rating: 1,
+    title: "1 out of 10",
+  },
+];
+
+export default ratings;
