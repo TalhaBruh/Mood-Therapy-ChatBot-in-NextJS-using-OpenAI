6a4ca346c639c826dd5e5b6fad7b9d93553ba954
Talha Hussain <talhahussain349@gmail.com>
2024-05-24 17:35:32 +0500

    Add predefined GPT response types
    - Defined GptResponseType to structure GPT response options
    - Added a list of response types with id and title properties

---
+++ utils/gptResponse.tsx
@@ -0,0 +1,10 @@
+type GptResponseType = {
+  id: number;
+  title: string;
+};
+
+export const gptResponse: GptResponseType[] = [
+  { id: 1, title: "short" },
+  { id: 2, title: "medium" },
+  { id: 3, title: "detailed" },
+];
