52199e767e6749d5f9b14145b64183f19acf4349
Talha Hussain <talhahussain349@gmail.com>
2024-06-12 01:59:07 +0500

    sspecific user

--- utils/actions.tsx
+++ utils/actions.tsx
@@ -143,7 +143,7 @@ export const updateUserAction = async (
     // Connecting to the DB
     await connectDB();
 
-    // Finding the specific user and updating the mood & session values
+    // Finding the Specific user and updating the mood & session values
     const user = await User.findOneAndUpdate(
       { userName: values.userName },
       {
