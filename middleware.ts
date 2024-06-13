7daa946fb1ddbd5cb6a3d89e323a3e65574859aa
Talha Hussain <talhahussain349@gmail.com>
2024-06-14 02:25:54 +0500

    checking func for route

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ const protectedRoutes = ["/dashboard"];
 const publicRoutes = ["/login", "/register", "/mood", "/session"];
 
 const middleware = async (req: NextRequest) => {
-  // 2. Check if the current Route is protected or public
+  // 2. Check if the current Route is protected or Public
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
